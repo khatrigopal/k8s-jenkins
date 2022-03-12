9c8476c1a67c1cf3b4b3a14c1a2a96213a8ef10a
Gopal Khatri <khatri.gopal@gmail.com>
2022-03-12 17:29:51 +0000

    done

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('This is Cloud Technolog in HCL Technologye\n');
+  res.send('This is Cloud Technolog in HCL Technology Ltde\n');
 });
 
 app.listen(PORT);
